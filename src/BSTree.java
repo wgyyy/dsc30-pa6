7ed7ded2b016c409d3b199faa9f3bd17d73f799e
unknown <741948314@qq.com>
2022-02-11 22:20:19 -0800

    cp

---
+++ src/BSTree.java
@@ -0,0 +1,401 @@
+/*
+ * Name: Gaoying Wang
+ * PID:  A16131629
+ */
+
+import java.util.*;
+
+/**
+ * Binary search tree implementation.
+ * 
+ * @author Gaoying Wang
+ * @since  ${2022-0210}
+ */
+public class BSTree<T extends Comparable<? super T>> implements Iterable {
+
+    /* * * * * BST Instance Variables * * * * */
+
+    private int nelems; // number of elements stored
+    private BSTNode root; // reference to root node
+
+    /* * * * * BST Node Inner Class * * * * */
+
+    protected class BSTNode {
+
+        T key;
+        LinkedList<T> dataList;
+        BSTNode left;
+        BSTNode right;
+
+        /**
+         * A constructor that initializes the BSTNode instance variables.
+         *
+         * @param left     Left child
+         * @param right    Right child
+         * @param dataList Linked list of related info
+         * @param key      Node's key
+         */
+        public BSTNode(BSTNode left, BSTNode right, LinkedList<T> dataList, T key) {
+            this.key = key;
+            this.dataList = dataList;
+            this.left = left;
+            this.right = right;
+        }
+
+        /**
+         * A constructor that initializes BSTNode variables. Note: This constructor is
+         * used when you want to add a key with no related information yet. In this
+         * case, you must create an empty LinkedList for the node.
+         *
+         * @param left  Left child
+         * @param right Right child
+         * @param key   Node's key
+         */
+        public BSTNode(BSTNode left, BSTNode right, T key) {
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            this.dataList=new LinkedList<T>();
+        }
+
+        /**
+         * Return the key
+         *
+         * @return The key
+         */
+        public T getKey() {
+            return this.key;
+        }
+
+        /**
+         * Return the left child of the node
+         *
+         * @return The left child of the node
+         */
+        public BSTNode getLeft() {
+            return this.left;
+        }
+
+        /**
+         * Return the right child of the node
+         *
+         * @return The right child of the node
+         */
+        public BSTNode getRight() {
+            return this.right;
+        }
+
+        /**
+         * Return the linked list of the node
+         *
+         * @return The linked list of the node
+         */
+        public LinkedList<T> getDataList() {
+            return this.dataList;
+        }
+
+        /**
+         * Setter for left child of the node
+         *
+         * @param newleft New left child
+         */
+        public void setleft(BSTNode newleft) {
+            this.left=newleft;
+        }
+
+        /**
+         * Setter for right child of the node
+         *
+         * @param newright New right child
+         */
+        public void setright(BSTNode newright) {
+            this.right=newright;
+        }
+
+        /**
+         * Setter for the linked list of the node
+         *
+         * @param newData New linked list
+         */
+        public void setDataList(LinkedList<T> newData) {
+            this.dataList=newData;
+        }
+
+        /**
+         * Append new data to the end of the existing linked list of the node
+         *
+         * @param data New data to be appended
+         */
+        public void addNewInfo(T data) {
+            this.dataList.add(data);
+        }
+
+        /**
+         * Remove 'data' from the linked list of the node and return true. If the linked
+         * list does not contain the value 'data', return false.
+         *
+         * @param data Info to be removed
+         * @return True if data was found, false otherwise
+         */
+        public boolean removeInfo(T data) {
+            return this.dataList.remove(data);
+        }
+
+    }
+
+    /* * * * * BST Methods * * * * */
+
+    /**
+     * 0-arg constructor that initializes root to null and nelems to 0
+     */
+    public BSTree() {
+        this.root=null;
+        this.nelems=0;
+    }
+
+    /**
+     * Return the root of BSTree. Returns null if the tree is empty
+     *
+     * @return The root of BSTree, null if the tree is empty
+     */
+    public BSTNode getRoot() {
+        if (this.nelems==0){
+            return null;
+        }else{
+            return this.root;
+        }
+    }
+
+    /**
+     * Return the BST size
+     *
+     * @return The BST size
+     */
+    public int getSize() {
+        return this.nelems;
+    }
+
+    private void nodeInsertion(BSTNode insertNode, BSTNode input){
+        if (insertNode.getKey().compareTo(input.getKey())<0){
+            if (input.getLeft()==null) {
+                input.setleft(insertNode);
+            } else {
+                nodeInsertion(insertNode,input.getLeft());
+            }
+        }else{
+            if (input.getRight()==null) {
+                input.setright(insertNode);
+            } else {
+                nodeInsertion(insertNode,input.getRight());
+            }
+        }
+    }
+
+    private boolean FindNode(BSTNode target, BSTNode input){
+        if (input.getLeft() == null && input.getRight() == null){
+            if (input.getKey() == target.getKey()){
+                return true;
+            }else{
+                return false;
+            }
+        } else {
+            if (target.getKey().compareTo(input.getKey())<0){
+                if(input.getLeft()==null){
+                    return false;
+                }else{
+                    return FindNode(target,input.getLeft());
+                }
+            }else{
+                if(input.getRight()==null){
+                    return false;
+                }else{
+                    return FindNode(target,input.getRight());
+                }
+            }
+        }
+    }
+
+    private BSTNode GetNode(T target, BSTNode input){
+        if (input.getKey().compareTo(target)==0){
+            return input;
+        }else {
+            if (input.getKey().compareTo(target)<0){
+                return GetNode(target,input.getLeft());
+            }else{
+                return GetNode(target,input.getRight());
+            }
+        }
+    }
+
+    private void setRoot(T key){
+        BSTNode newNode=new BSTNode(null,null,key);
+        this.root= newNode;
+    }
+
+
+    /**
+     * Insert a key into BST
+     * 
+     * @param key
+     * @return true if insertion is successful and false otherwise
+     */
+    public boolean insert(T key) {
+        if (key == null){
+            throw new NullPointerException();
+        }
+        BSTNode newNode = new BSTNode(null, null, key);
+        if (this.getRoot()==null) {
+            this.setRoot(key);
+            this.nelems++;
+            return true;
+        }else if (this.findKey(key)) {
+            return false;
+        } else {
+            nodeInsertion(newNode, this.getRoot());
+            this.nelems++;
+            return true;
+        }
+
+    }
+
+
+    /**
+     * Return true if the 'key' is found in the tree, false otherwise
+     *
+     * @param key To be searched
+     * @return True if the 'key' is found, false otherwise
+     * @throws NullPointerException If key is null
+     */
+    public boolean findKey(T key) {
+        if (key == null){
+            throw new NullPointerException();
+        }else {
+            BSTNode newNode = new BSTNode(null, null, key);
+            return this.FindNode(newNode, this.getRoot());
+        }
+    }
+
+    /**
+     * Insert 'data' into the LinkedList of the node whose key is 'key'
+     *
+     * @param key  Target key
+     * @param data To be added to key's LinkedList
+     * @throws NullPointerException     If eaither key or data is null
+     * @throws IllegalArgumentException If key is not found in the BST
+     */
+    public void insertData(T key, T data) {
+        if(!this.findKey(key)){
+            throw new IllegalArgumentException();
+        } else if (key == null || data == null){
+            throw new NullPointerException();
+        } else{
+            BSTNode N=this.GetNode(key, this.getRoot());
+            N.getDataList().add(data);
+        }
+    }
+
+    /**
+     * Return the LinkedList of the node with key value 'key'
+     *
+     * @param key Target key
+     * @return LinkedList of the node whose key value is 'key'
+     * @throws NullPointerException     If key is null
+     * @throws IllegalArgumentException If key is not found in the BST
+     */
+    public LinkedList<T> findDataList(T key) {
+        if (key == null){
+            throw new NullPointerException();
+        }else if(!this.findKey(key)){
+            throw new IllegalArgumentException();
+        }else{
+            BSTNode N=this.GetNode(key, this.getRoot());
+            return N.getDataList();
+        }
+    }
+
+    /**
+     * Return the height of the tree
+     *
+     * @return The height of the tree, -1 if BST is empty
+     */
+    public int findHeight() {
+        return findHeightHelper(this.root);
+    }
+
+    /**
+     * Helper for the findHeight method
+     *
+     * @param root Root node
+     * @return The height of the tree, -1 if BST is empty
+     */
+    private int findHeightHelper(BSTNode root) {
+        if (root == null){
+            return -1;
+        }
+        int left = findHeightHelper(root.left);
+        int right = findHeightHelper(root.right);
+        if (left > right){
+            return left + 1;
+        }else{
+            return right + 1;
+        }
+    }
+
+    /* * * * * BST Iterator * * * * */
+
+    public class BSTree_Iterator implements Iterator<T> {
+        public Stack<BSTNode> Ite;
+        public BSTree_Iterator() {
+            this.Ite=new Stack();
+            BSTNode current=root;
+            if (root!=null) {
+                while (current.left != null) {
+                    Ite.push(current);
+                    current = current.left;
+                }
+                Ite.push(current);
+            }
+        }
+
+        public boolean hasNext() {
+            if(nelems==0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        public T next() {
+            if (!this.hasNext()){
+                throw new NoSuchElementException();
+            }
+            BSTNode return_Node=this.Ite.pop();
+            T return_value=return_Node.getKey();
+            if (!(return_Node.getRight()==null)){
+                this.Ite.push(return_Node.getRight());
+                return_Node=return_Node.getRight();
+                while (!(return_Node.getLeft()==null)){
+                    this.Ite.push(return_Node.getLeft());
+                    return_Node=return_Node.getLeft();
+                }
+            }
+            return return_value;
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return new BSTree_Iterator();
+    }
+
+    /* * * * * Extra Credit Methods * * * * */
+
+    public ArrayList<T> intersection(Iterator<T> iter1, Iterator<T> iter2) {
+        /* TODO */
+        return null;
+    }
+
+    public T levelMax(int level) {
+        /* TODO */
+        return null;
+    }
+}
---
+++ src/BSTreeTest.java
@@ -0,0 +1,181 @@
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class BSTreeTest {
+
+    @org.junit.Test
+    public void getRoot() {
+        BSTree test=new BSTree();
+        assertEquals(null,test.getRoot());
+        test.insert(10);
+        assertEquals(10,test.getRoot().getKey());
+        test.insert(11);
+        assertEquals(10,test.getRoot().getKey());
+        BSTree test2=new BSTree();
+        test2.insert("haha");
+        assertEquals("haha",test2.getRoot().getKey());
+    }
+
+    @org.junit.Test
+    public void getSize() {
+        BSTree test=new BSTree();
+        assertEquals(0,test.getSize());
+        test.insert(10);
+        assertEquals(1,test.getSize());
+        test.insert(11);
+        assertEquals(2, test.getSize());
+        test.insert(11);
+        assertEquals(2, test.getSize());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void insertNPE(){
+        BSTree test=new BSTree();
+        test.insert(null);
+    }
+
+    @org.junit.Test
+    public void insert() {
+        BSTree test=new BSTree();
+        test.insert(10);
+        assertEquals(1,test.getSize());
+        test.insert(11);
+        assertEquals(2, test.getSize());
+        test.insert(11);
+        assertEquals(2, test.getSize());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void findKeyNPE(){
+        BSTree test=new BSTree();
+        test.findKey(null);
+    }
+
+    @org.junit.Test
+    public void findKey() {
+        BSTree test=new BSTree();
+        test.insert(10);
+        assertEquals(true,test.findKey(10));
+        test.insert(11);
+        assertEquals(true, test.findKey(11));
+        BSTree test2 = new BSTree();
+        test2.insert("hahaha");
+        assertEquals(true, test2.findKey("hahaha"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void insertDATANPE1(){
+        BSTree test=new BSTree();
+        test.insertData(null,1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void insertDATANPE2(){
+        BSTree test=new BSTree();
+        test.insert(10);
+        test.insertData(10,null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertDATAIAE(){
+        BSTree test=new BSTree();
+        test.insert(10);
+        test.insertData(2,1);
+    }
+
+
+    @org.junit.Test
+    public void insertData() {
+        BSTree test=new BSTree();
+        test.insert(10);
+        test.insertData(10, 1);
+        LinkedList testList=new LinkedList();
+        testList.add(1);
+        assertEquals(testList,test.findDataList(10));
+        test.insertData(10,2);
+        testList.add(2);
+        assertEquals(testList,test.findDataList(10));
+        BSTree test2=new BSTree();
+        test2.insert("haha");
+        test2.insertData("haha","yeah!");
+        LinkedList testList2=new LinkedList();
+        testList2.add("yeah!");
+        assertEquals(testList2,test2.findDataList("haha"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void findDataListNPE(){
+        BSTree test=new BSTree();
+        test.findDataList(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void findDataListIAE(){
+        BSTree test=new BSTree();
+        test.insert(10);
+        test.findDataList(100);
+    }
+
+    @org.junit.Test
+    public void findDataList() {
+        BSTree test=new BSTree();
+        LinkedList testList=new LinkedList();
+        test.insert(10);
+        assertEquals(testList,test.findDataList(10));
+        test.insertData(10,1);
+        testList.add(1);
+        assertEquals(testList,test.findDataList(10));
+        BSTree test2=new BSTree();
+        test2.insert("haha");
+        test2.insertData("haha","yeah!");
+        LinkedList testList2=new LinkedList();
+        testList2.add("yeah!");
+        assertEquals(testList2,test2.findDataList("haha"));
+    }
+
+    @org.junit.Test
+    public void findHeight() {
+        BSTree test=new BSTree();
+        assertEquals(-1,test.findHeight());
+        test.insert(10);
+        assertEquals(0,test.findHeight());
+        test.insert(5);
+        assertEquals(1,test.findHeight());
+        test.insert(11);
+        test.insert(3);
+        test.insert(1);
+        assertEquals(3,test.findHeight());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorNSEE(){
+        BSTree test=new BSTree();
+        Iterator<Integer> iter1=test.iterator();
+        iter1.next();
+    }
+
+
+    @org.junit.Test
+    public void iterator() {
+        BSTree test=new BSTree();
+        test.insert(10);
+        test.insert(5);
+        test.insert(11);
+        test.insert(3);
+        test.insert(1);
+        Iterator<Integer> iter1=test.iterator();
+        int actual=iter1.next();
+        assertEquals(1,actual);
+        actual=iter1.next();
+        assertEquals(3,actual);
+        iter1.next();
+        iter1.next();
+        actual=iter1.next();
+        assertEquals(11,actual);
+    }
+}
